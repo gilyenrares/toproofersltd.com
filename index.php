f03014f3938304d939df1f096fed5e3c6f7df502
gilyenrares <gilyenrares@users.noreply.github.com>
2020-01-17 20:10:58 +0000

    navbar

--- index.php
+++ index.php
@@ -9,7 +9,36 @@
 	<link rel="stylesheet" type="text/css" href="styles/style.css">
 </head>
 <body>
-	<!-- Navigation -->
-	<?php include 'sections/navigation.sec.php';?>
-			
+<!-- Navigation -->
+<img src="images/roof.png" width="100%">
+<?php include 'sections/navigation.sec.php';?>
+<!-- Carousel with images -->
+<div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/1.jpg" class="d-block w-50 mr-auto ml-auto" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/2.jpg" class="d-block w-50 mr-auto ml-auto" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/3.jpg" class="d-block w-50 mr-auto ml-auto" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon bg-red" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon bg-red" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+</html>
